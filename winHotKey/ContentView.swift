0d945cf35119097597fb6830f14aaf68e7a6629e
jackyoung022 <jackyoung022@gmail.com>
2025-10-15 21:24:52 +0800

    Initial Commit

---
+++ winHotKey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  winHotKey
+//
+//  Created by jack on 2025/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ winHotKey/winHotKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  winHotKeyApp.swift
+//  winHotKey
+//
+//  Created by jack on 2025/10/15.
+//
+
+import SwiftUI
+
+@main
+struct winHotKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
